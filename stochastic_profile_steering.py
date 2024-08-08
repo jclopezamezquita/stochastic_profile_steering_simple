d659da0fa43fce654238fb4f34bc46191ff2fdc8
López Amézquita <j.c.lopezamezquita@utwente.nl>
2024-08-08 10:07:05 +0200

    Triggers SPS with limit only if there are limit violations

--- stochastic_profile_steering.py
+++ stochastic_profile_steering.py
@@ -510,120 +510,45 @@ def stochastic_profile_steering(time_data_file, agents_data_file, parameters_fil
     ######################################################### Lines 2 - 3 - Algorithm 3 #####################################################################
     ######################################################### Check probabilistic grid limits ###############################################################
 
-    if results['of'] <= 0.1:
-        return results
+    # Verifies if there are limit violations
+    if results['of'] > 0.1:
 
-    # Call algorithm 2
-    print("############## Starting Algorithm 2 ######################")
-    initial_results = stochastic_profile_steering_with_limits(time_data_file, agents_data_file, parameters_file)
-    print("############## Algorithm 2 has finished ######################")
+        # Call algorithm 2
+        print("############## Starting Algorithm 2 ######################")
+        initial_results = stochastic_profile_steering_with_limits(time_data_file, agents_data_file, parameters_file)
+        print("############## Algorithm 2 has finished ######################")
 
-    # Estimate individual grid limits
-    for agent in data_agents:
-        for time in data_time:
-            LIMIT_UP = 0.0
-            LIMIT_DOWN = 0.0
-            if initial_results["Aggregated"]['x_demand_avg'][time] + 2 * math.sqrt(initial_results["Aggregated"]['x_demand_std'][time]) > LIMIT:
-                LIMIT_UP = initial_results["Aggregated"]['x_demand_avg'][time] + 2 * math.sqrt(initial_results["Aggregated"]['x_demand_std'][time])
-            else:
-                LIMIT_UP = LIMIT
-            if -LIMIT > initial_results["Aggregated"]['x_demand_avg'][time] - 2 * math.sqrt(initial_results["Aggregated"]['x_demand_std'][time]):
-                LIMIT_DOWN = -initial_results["Aggregated"]['x_demand_avg'][time] + 2 * math.sqrt(initial_results["Aggregated"]['x_demand_std'][time])
-            else:
-                LIMIT_DOWN = LIMIT
-
-            # results[agent]['upper_individual_limit'][time] = round(LIMIT_UP - (initial_results["Aggregated"]['x_demand_avg'][time] - initial_results[agent]['x_demand_avg'][time]) - 2 * math.sqrt(max(0,initial_results["Aggregated"]['x_demand_std'][time] - round(initial_results[agent]['x_demand_std'][time]**2,2))),2)
-            # results[agent]['lower_individual_limit'][time] = round(LIMIT_DOWN + (initial_results["Aggregated"]['x_demand_avg'][time] - initial_results[agent]['x_demand_avg'][time]) - 2 * math.sqrt(max(0,initial_results["Aggregated"]['x_demand_std'][time] - round(initial_results[agent]['x_demand_std'][time]**2,2))),2)
-            results[agent]['upper_individual_limit'][time] = round(LIMIT_UP - (initial_results["Aggregated"]['x_demand_avg'][time] - initial_results[agent]['x_demand_avg'][time]) - 2 * max(0,math.sqrt(initial_results["Aggregated"]['x_demand_std'][time]) - round(initial_results[agent]['x_demand_std'][time],2)),2)
-            results[agent]['lower_individual_limit'][time] = round(LIMIT_DOWN + (initial_results["Aggregated"]['x_demand_avg'][time] - initial_results[agent]['x_demand_avg'][time]) - 2 * max(0,math.sqrt(initial_results["Aggregated"]['x_demand_std'][time]) - round(initial_results[agent]['x_demand_std'][time],2)),2)
-        # print(results[agent]['upper_individual_limit'])
-        # print(results[agent]['lower_individual_limit'])
-    
-    print("############## Starting Algorithm 3 - SPS ######################")
-
-    # Update state variables and of
-    results['of'] = 0.0
-    for time in data_time:
-        results["Aggregated"]['x_demand_avg'][time] = 0.0
-        results["Aggregated"]['x_demand_std'][time] = 0.0
+        # Estimate individual grid limits
         for agent in data_agents:
-            results[agent]['x_demand_avg'][time] = initial_results[agent]['x_demand_avg'][time]
-            results[agent]['x_demand_std'][time] = initial_results[agent]['x_demand_std'][time]
-            results["Aggregated"]['x_demand_avg'][time] = results["Aggregated"]['x_demand_avg'][time] + results[agent]['x_demand_avg'][time]
-            results["Aggregated"]['x_demand_std'][time] = results["Aggregated"]['x_demand_std'][time] + results[agent]['x_demand_std'][time]**2
-        # results["Aggregated"]['x_demand_std'][time] = math.sqrt(results["Aggregated"]['x_demand_std'][time])
-        results["Aggregated"]['x_demand_avg'][time] = round(results["Aggregated"]['x_demand_avg'][time],2)
-        results["Aggregated"]['x_demand_std'][time] = round(results["Aggregated"]['x_demand_std'][time],2)
-        results['of'] = results['of'] + results["Aggregated"]['x_demand_avg'][time]**2
-    results['of'] = round(results['of'],2)
-    
-    ######################################################### Line 8 - Algorithm 3 #####################################################################
-    ######################################################### Initialize improvement ###################################################################
-
-    total_improvement = 1000000
-
-    ######################################################### Line 9 - Algorithm 3 ####################################################################
-    ######################################################### while loop ##############################################################################
-
-    iteration = 0
-    while total_improvement > TOLERANCE:
-        iteration = iteration + 1
-
-        ######################################################### Line 10 - Algorithm 3 ####################################################################
-        ######################################################### for loop ################################################################################
-
-        improvement_per_agent = {}
-        for agent in data_agents:
-            improvement_per_agent[agent] = 0.0
-
-            ######################################################### Lines 11-12 - Algorithm 3 ####################################################################
-            ######################################################### Optimize average consumption per agent ##################################################
-
-            print("Agent " + agent + ":")
-            x_demand_avg, x_demand_std, of = household_EMS.household_EMS_total(delta_T=DELTA_TIME, agent_name=agent, data_time=data_time, data_agent=data_agents[agent], aggregated_demand_avg=results["Aggregated"]['x_demand_avg'], demand_agent_avg=results[agent]['x_demand_avg'], aggregated_demand_std=results["Aggregated"]['x_demand_std'], demand_agent_std=results[agent]['x_demand_std'], lower_limit_agent=results[agent]['lower_individual_limit'], upper_limit_agent=results[agent]['upper_individual_limit'], grid_limit=LIMIT)
-
-            # Save results
-            results[agent]['of_temporal'] = of
             for time in data_time:
-                results[agent]['x_demand_avg_temporal'][time] = x_demand_avg[time]
-                results[agent]['x_demand_std_temporal'][time] = x_demand_std[time]
+                LIMIT_UP = 0.0
+                LIMIT_DOWN = 0.0
+                if initial_results["Aggregated"]['x_demand_avg'][time] + 2 * math.sqrt(initial_results["Aggregated"]['x_demand_std'][time]) > LIMIT:
+                    LIMIT_UP = initial_results["Aggregated"]['x_demand_avg'][time] + 2 * math.sqrt(initial_results["Aggregated"]['x_demand_std'][time])
+                else:
+                    LIMIT_UP = LIMIT
+                if -LIMIT > initial_results["Aggregated"]['x_demand_avg'][time] - 2 * math.sqrt(initial_results["Aggregated"]['x_demand_std'][time]):
+                    LIMIT_DOWN = -initial_results["Aggregated"]['x_demand_avg'][time] + 2 * math.sqrt(initial_results["Aggregated"]['x_demand_std'][time])
+                else:
+                    LIMIT_DOWN = LIMIT
 
-            ######################################################### Line 13 - Algorithm 3 ####################################################################
-            ######################################################### Calculate improvement per agent  ########################################################
-
-            improvement_per_agent[agent] = round(results['of'] - results[agent]['of_temporal'],2)
+                # results[agent]['upper_individual_limit'][time] = round(LIMIT_UP - (initial_results["Aggregated"]['x_demand_avg'][time] - initial_results[agent]['x_demand_avg'][time]) - 2 * math.sqrt(max(0,initial_results["Aggregated"]['x_demand_std'][time] - round(initial_results[agent]['x_demand_std'][time]**2,2))),2)
+                # results[agent]['lower_individual_limit'][time] = round(LIMIT_DOWN + (initial_results["Aggregated"]['x_demand_avg'][time] - initial_results[agent]['x_demand_avg'][time]) - 2 * math.sqrt(max(0,initial_results["Aggregated"]['x_demand_std'][time] - round(initial_results[agent]['x_demand_std'][time]**2,2))),2)
+                results[agent]['upper_individual_limit'][time] = round(LIMIT_UP - (initial_results["Aggregated"]['x_demand_avg'][time] - initial_results[agent]['x_demand_avg'][time]) - 2 * max(0,math.sqrt(initial_results["Aggregated"]['x_demand_std'][time]) - round(initial_results[agent]['x_demand_std'][time],2)),2)
+                results[agent]['lower_individual_limit'][time] = round(LIMIT_DOWN + (initial_results["Aggregated"]['x_demand_avg'][time] - initial_results[agent]['x_demand_avg'][time]) - 2 * max(0,math.sqrt(initial_results["Aggregated"]['x_demand_std'][time]) - round(initial_results[agent]['x_demand_std'][time],2)),2)
+            # print(results[agent]['upper_individual_limit'])
+            # print(results[agent]['lower_individual_limit'])
         
-        ######################################################### Line 15 - Algorithm 3 ####################################################################
-        ######################################################### Determine the best agent based on improvements  #########################################
+        print("############## Starting Algorithm 3 - SPS ######################")
 
-        best_improvement_per_agent = 0.0
-        best_agent = None
-        for agent in data_agents:
-            if best_improvement_per_agent < improvement_per_agent[agent]:
-                best_improvement_per_agent = improvement_per_agent[agent]
-                best_agent = agent
-        if best_agent is None:
-            print("Best agent - Algorithm 3: None")
-            break
-        else:
-            print("Best agent - Algorithm 3: " + best_agent)
-            total_improvement = best_improvement_per_agent
-            print("Total improvement: " + str(total_improvement))
-
-        ######################################################### Line 16 - Algorithm 3 ####################################################################
-        ######################################################### Update the profile of the best agent  ####################################################
-
-        # Update best agent profile anf OF
-        for time in data_time:
-            results[best_agent]['x_demand_avg'][time] = results[best_agent]['x_demand_avg_temporal'][time]
-            results[best_agent]['x_demand_std'][time] = results[best_agent]['x_demand_std_temporal'][time]
-
-        # Update aggregated profile
+        # Update state variables and of
         results['of'] = 0.0
         for time in data_time:
             results["Aggregated"]['x_demand_avg'][time] = 0.0
             results["Aggregated"]['x_demand_std'][time] = 0.0
             for agent in data_agents:
+                results[agent]['x_demand_avg'][time] = initial_results[agent]['x_demand_avg'][time]
+                results[agent]['x_demand_std'][time] = initial_results[agent]['x_demand_std'][time]
                 results["Aggregated"]['x_demand_avg'][time] = results["Aggregated"]['x_demand_avg'][time] + results[agent]['x_demand_avg'][time]
                 results["Aggregated"]['x_demand_std'][time] = results["Aggregated"]['x_demand_std'][time] + results[agent]['x_demand_std'][time]**2
             # results["Aggregated"]['x_demand_std'][time] = math.sqrt(results["Aggregated"]['x_demand_std'][time])
@@ -631,13 +556,88 @@ def stochastic_profile_steering(time_data_file, agents_data_file, parameters_fil
             results["Aggregated"]['x_demand_std'][time] = round(results["Aggregated"]['x_demand_std'][time],2)
             results['of'] = results['of'] + results["Aggregated"]['x_demand_avg'][time]**2
         results['of'] = round(results['of'],2)
-
-        print("Calculating updated average profile...")
-
-        print("Average profile: " + str([round(results["Aggregated"]['x_demand_avg'][time],2) for time in data_time]))
-        print("Std. deviation: " + str([round(math.sqrt(results["Aggregated"]['x_demand_std'][time]),2) for time in data_time]))
-        print("Iteration " + str(iteration) + ", updated OF - Algorithm 3: " + str(results['of']))
         
+        ######################################################### Line 8 - Algorithm 3 #####################################################################
+        ######################################################### Initialize improvement ###################################################################
+
+        total_improvement = 1000000
+
+        ######################################################### Line 9 - Algorithm 3 ####################################################################
+        ######################################################### while loop ##############################################################################
+
+        iteration = 0
+        while total_improvement > TOLERANCE:
+            iteration = iteration + 1
+
+            ######################################################### Line 10 - Algorithm 3 ####################################################################
+            ######################################################### for loop ################################################################################
+
+            improvement_per_agent = {}
+            for agent in data_agents:
+                improvement_per_agent[agent] = 0.0
+
+                ######################################################### Lines 11-12 - Algorithm 3 ####################################################################
+                ######################################################### Optimize average consumption per agent ##################################################
+
+                print("Agent " + agent + ":")
+                x_demand_avg, x_demand_std, of = household_EMS.household_EMS_total(delta_T=DELTA_TIME, agent_name=agent, data_time=data_time, data_agent=data_agents[agent], aggregated_demand_avg=results["Aggregated"]['x_demand_avg'], demand_agent_avg=results[agent]['x_demand_avg'], aggregated_demand_std=results["Aggregated"]['x_demand_std'], demand_agent_std=results[agent]['x_demand_std'], lower_limit_agent=results[agent]['lower_individual_limit'], upper_limit_agent=results[agent]['upper_individual_limit'], grid_limit=LIMIT)
+
+                # Save results
+                results[agent]['of_temporal'] = of
+                for time in data_time:
+                    results[agent]['x_demand_avg_temporal'][time] = x_demand_avg[time]
+                    results[agent]['x_demand_std_temporal'][time] = x_demand_std[time]
+
+                ######################################################### Line 13 - Algorithm 3 ####################################################################
+                ######################################################### Calculate improvement per agent  ########################################################
+
+                improvement_per_agent[agent] = round(results['of'] - results[agent]['of_temporal'],2)
+            
+            ######################################################### Line 15 - Algorithm 3 ####################################################################
+            ######################################################### Determine the best agent based on improvements  #########################################
+
+            best_improvement_per_agent = 0.0
+            best_agent = None
+            for agent in data_agents:
+                if best_improvement_per_agent < improvement_per_agent[agent]:
+                    best_improvement_per_agent = improvement_per_agent[agent]
+                    best_agent = agent
+            if best_agent is None:
+                print("Best agent - Algorithm 3: None")
+                break
+            else:
+                print("Best agent - Algorithm 3: " + best_agent)
+                total_improvement = best_improvement_per_agent
+                print("Total improvement: " + str(total_improvement))
+
+            ######################################################### Line 16 - Algorithm 3 ####################################################################
+            ######################################################### Update the profile of the best agent  ####################################################
+
+            # Update best agent profile anf OF
+            for time in data_time:
+                results[best_agent]['x_demand_avg'][time] = results[best_agent]['x_demand_avg_temporal'][time]
+                results[best_agent]['x_demand_std'][time] = results[best_agent]['x_demand_std_temporal'][time]
+
+            # Update aggregated profile
+            results['of'] = 0.0
+            for time in data_time:
+                results["Aggregated"]['x_demand_avg'][time] = 0.0
+                results["Aggregated"]['x_demand_std'][time] = 0.0
+                for agent in data_agents:
+                    results["Aggregated"]['x_demand_avg'][time] = results["Aggregated"]['x_demand_avg'][time] + results[agent]['x_demand_avg'][time]
+                    results["Aggregated"]['x_demand_std'][time] = results["Aggregated"]['x_demand_std'][time] + results[agent]['x_demand_std'][time]**2
+                # results["Aggregated"]['x_demand_std'][time] = math.sqrt(results["Aggregated"]['x_demand_std'][time])
+                results["Aggregated"]['x_demand_avg'][time] = round(results["Aggregated"]['x_demand_avg'][time],2)
+                results["Aggregated"]['x_demand_std'][time] = round(results["Aggregated"]['x_demand_std'][time],2)
+                results['of'] = results['of'] + results["Aggregated"]['x_demand_avg'][time]**2
+            results['of'] = round(results['of'],2)
+
+            print("Calculating updated average profile...")
+
+            print("Average profile: " + str([round(results["Aggregated"]['x_demand_avg'][time],2) for time in data_time]))
+            print("Std. deviation: " + str([round(math.sqrt(results["Aggregated"]['x_demand_std'][time]),2) for time in data_time]))
+            print("Iteration " + str(iteration) + ", updated OF - Algorithm 3: " + str(results['of']))
+            
     ######################################################### Line 18 - Algorithm 3 ####################################################################
     ######################################################### Show the optimized profile  ####################################################
 
