7524097e8c1ed9c78324713f1191cee9daf0d2f5
López Amézquita <j.c.lopezamezquita@utwente.nl>
2024-08-08 11:59:26 +0200

    Better names for input data

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import shutil
 from tools import stochastic_profile_steering
 
 # Define the time_data and agents_data files
-time_data_file = 'input_data/data_15min/time_data_15min.json'
-agents_data_file = 'input_data/data_15min/agents_data_15min.json'
-parameters_file = 'input_data/data_15min/parameters_15min.json'
+time_data_file = 'input_data/EC_with_3households_data_15min/time_data_15min.json'
+agents_data_file = 'input_data/EC_with_3households_data_15min/agents_data_15min.json'
+parameters_file = 'input_data/EC_with_3households_data_15min/parameters_15min.json'
+# time_data_file = 'input_data/EC_with_3households_data_1hour/time_data_1hour.json'
+# agents_data_file = 'input_data/EC_with_3households_data_1hour/agents_data_1hour.json'
+# parameters_file = 'input_data/EC_with_3households_data_1hour/parameters_1hour.json'
 
 # Create Estudos/ if not exists
 if not os.path.exists('results'):
